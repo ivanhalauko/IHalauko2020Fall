62b02bd161fe55acff306c4af25709d1fb470372
IvanHalauko <van_delta@mail.ru>
2020-10-18 23:27:45 +0300

    0.FirstTask 5.Created Store class and tests ShapesStorageTests class for this class.

---
+++ TaskFirst/src/ShapesStorage/Store.cs
@@ -0,0 +1,128 @@
+﻿using Shapes.Model.Lib.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShapesStorage
+{
+    public class Store
+    {
+		/// <summary>
+		/// List of abstract figures.
+		/// </summary>
+		public List<BaseShape> Figures { get; set; }
+
+		/// <summary>
+		/// Constructor with parameter.
+		/// </summary>
+		/// <param name="figures"></param>
+		public Store(List<BaseShape> figures)
+		{
+			Figures = figures;
+		}
+
+		/// <summary>
+		/// Constructor without parameters.
+		/// </summary>
+		public Store() : this(new List<BaseShape>())
+		{
+		}
+
+		/// <summary>
+		/// Method for adding figures to list.
+		/// </summary>
+		/// <param name="figure"></param>
+		public void AddShape(BaseShape figure)
+		{
+			if (Figures.Contains(figure))
+				throw new Exception("Figure of this type are already in the store");
+
+			if (Figures.Count == 20)
+				throw new Exception("There is no free space in the store");
+
+			Figures.Add(figure);
+		}
+
+		/// <summary>
+		/// Method for summation perimeters of all figures in the list.
+		/// </summary>
+		/// <returns></returns>
+		public double SumAllFiguresPerimeters()
+		{
+			double sum = 0;
+			foreach (BaseShape item in Figures)
+			{
+				sum += item.Perimeter;
+			}
+			return sum;
+		}
+
+		/// <summary>
+		/// Method for summation areas of all figures in the list.
+		/// </summary>
+		/// <returns></returns>
+		public double SumAllFiguresAreas()
+		{
+			double sum = 0;
+			foreach (BaseShape item in Figures)
+			{
+				sum += item.Area;
+			}
+			return sum;
+		}
+
+		/// <summary>
+		/// Method for calculation Average perimeters of all figures in the list.
+		/// </summary>
+		/// <returns></returns>
+		public double AverageAllFiguresPerimeters()
+		{
+			var countOfList = Figures.Count();
+			double sum = 0;
+			foreach (BaseShape item in Figures)
+			{
+				sum += item.Perimeter;
+			}
+			return sum / countOfList;
+		}
+
+		/// <summary>
+		/// Method for calculation Average areas of all figures in the list.
+		/// </summary>
+		/// <returns></returns>
+		public double AverageAllFiguresAreas()
+		{
+			var countOfList = Figures.Count();
+			double sum = 0;
+			foreach (BaseShape item in Figures)
+			{
+				sum += item.Area;
+			}
+			return sum / countOfList;
+		}
+
+		// <summary>
+		/// Comparison one list with another.
+		/// </summary>
+		/// <param name="obj">Comparer parameter.</param>
+		/// <returns>Return "true" if equal and "false" if not equal.</returns>
+		public override bool Equals(object obj)
+		{
+			if (obj == null || GetType() != obj.GetType())
+				return false;
+			Store r = (Store)obj;
+			return //Figures.Contains(r.Figures); 
+				   //Figures.Equals(r.Figures);
+				Enumerable.SequenceEqual(Figures, r.Figures);
+		}
+
+		/// <summary>
+		/// Calculate hash code for comparison one list with another.
+		/// </summary>
+		/// <returns>Hash code.</returns>
+		public override int GetHashCode()
+		{
+			return Tuple.Create(Figures).GetHashCode();
+		}
+	}
+}
