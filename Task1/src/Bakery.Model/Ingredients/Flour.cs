838e89de180c2565527b09b6f06a569b0555dfdf
IvanHalauko <van_delta@mail.ru>
2020-11-25 03:04:57 +0300

    Task1. 06.Created Butter and Flour Ingredient classes.

---
+++ Task1/src/Bakery.Model/Ingredients/Flour.cs
@@ -0,0 +1,19 @@
+﻿namespace Bakery.Model.Ingredients
+{
+	/// <summary>
+	/// Flour Ingredient class. 
+	/// </summary>
+	public class Flour : BaseAbstractIngredient
+	{
+		/// <summary>
+		/// Constuctor with parameter.
+		/// </summary>
+		/// <param name="weight">Ingredient's weight parameter.</param>
+		public Flour(double weight)
+		{
+			base.Weight = weight;
+			base.Cost = 200;
+			base.EnergyValue = 1380;
+		}
+	}
+}
