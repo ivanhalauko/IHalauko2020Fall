0704026fbba530e969b9707a3e75cba7b5e41c38
IvanHalauko <van_delta@mail.ru>
2020-11-25 03:41:53 +0300

    Task1. 10.Created IBakeryData interface.

---
+++ Task1/src/Bakery.Data/Interfaces/IBakeryData.cs
@@ -0,0 +1,16 @@
+﻿using Bakery.Model.Products;
+
+namespace Bakery.Data.Interfaces
+{
+	/// <summary>
+	/// IBakeryData interface.
+	/// </summary>
+	interface IBakeryData
+	{
+		/// <summary>
+		/// Get base abstract product array with product.
+		/// </summary>
+		/// <returns></returns>
+		BaseAbstractProduct[] GetProductArray();
+	}
+}
