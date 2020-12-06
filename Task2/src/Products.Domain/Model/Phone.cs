ec6734b138ed8b20254d30f1faf7dc74838c1ac0
IvanHalauko <van_delta@mail.ru>
2020-12-06 03:38:34 +0300

    Task2. 10.Added Phone methods and tests.

--- Task2/src/Products.Domain/Model/Phone.cs
+++ Task2/src/Products.Domain/Model/Phone.cs
@@ -19,5 +19,90 @@ namespace Products.Domain.Model
 		public Phone(string name, decimal cost, double markup, int quantity, string productType) : base(name, cost, markup, quantity, productType)
 		{
 		}
+
+		/// <summary>
+		/// The method overrides the mathematical "plus" operation for working with Products objects by cost, markup and quantity. Task's point №8.
+		/// </summary>
+		/// <param name="firstProduct">First product summand.</param>
+		/// <param name="secondProduct">Second product summand.</param>
+		/// <returns>New product result.</returns>
+		public static Phone operator +(Phone firstProduct, Phone secondProduct)
+		{
+			if (firstProduct == null)
+				throw new ArgumentNullException("Fist Phone is null");
+			if (secondProduct == null)
+				throw new ArgumentNullException("Second Phone two is null");
+			if (firstProduct.Name != secondProduct.Name)
+				throw new ArgumentException("Fist Phone's name is not equal second Phone's name.");
+			if (firstProduct.ProductType != secondProduct.ProductType)
+				throw new ArgumentException("Fist Phone's Type is not equal second Phone's Type.");
+
+			return new Phone(
+				name: firstProduct.Name,
+				cost: Math.Round((firstProduct.Cost * firstProduct.Quantity + secondProduct.Cost * secondProduct.Quantity) / (firstProduct.Quantity + secondProduct.Quantity), 3, MidpointRounding.AwayFromZero),
+				markup: Math.Round((firstProduct.Markup * firstProduct.Quantity + secondProduct.Markup * secondProduct.Quantity) / (firstProduct.Quantity + secondProduct.Quantity), 3, MidpointRounding.AwayFromZero),
+				quantity: firstProduct.Quantity + secondProduct.Quantity,
+				productType: firstProduct.ProductType
+				);
+		}
+
+		/// <summary>
+		/// The method overrides the mathematical "minus" operation for working with Products objects. Task's point №9.
+		/// </summary>
+		/// <param name="product">Product parameter.</param>
+		/// <param name="productsNumber">Number parameter.</param>
+		/// <returns>New product with new quantity result.</returns>
+		public static Phone operator -(Phone product, int productsNumber)
+		{
+			if (product == null)
+				throw new ArgumentNullException("Product is not null");
+
+			return new Phone(
+				name: product.Name,
+				cost: product.Cost,
+				markup: product.Markup,
+				quantity: product.Quantity - productsNumber,
+				productType: product.ProductType
+				);
+		}
+
+		/// <summary>
+		/// Convet Phone type to book. Task's point №10.
+		/// </summary>
+		/// <param name="product">Return book type.</param>
+		public static explicit operator Phone(Book product) => new Phone(product.Name, product.Cost, product.Markup, product.Quantity, product.ProductType);
+
+		/// <summary>
+		/// Override Equals method for comparing one book with another..
+		/// </summary>
+		/// <param name="obj">Object parameter.</param>
+		/// <returns>Return bool value.</returns>
+		public override bool Equals(object obj)
+		{
+			if (obj == null || GetType() != obj.GetType())
+				return false;
+			return obj is Phone phone &&
+			   base.Equals(obj) &&
+			   Name == phone.Name &&
+			   Cost == phone.Cost &&
+			   Markup == phone.Markup &&
+			   Quantity == phone.Quantity &&
+			   ProductType == phone.ProductType &&
+			   Price == phone.Price &&
+			   TotalPrice == phone.TotalPrice;
+		}
+
+		/// <summary>
+		/// Override GetHashCode method.
+		/// </summary>
+		/// <returns>Return hash code.</returns>
+		public override int GetHashCode() => Tuple.Create(base.GetHashCode()).GetHashCode();
+
+		/// <summary>
+		/// Override ToString method.
+		/// </summary>
+		/// <returns>Return string</returns>
+		public override string ToString() => string.Format("{0};{1};{2};{3};{4};{5};{6}",
+			base.ToString(), Name, Cost, Markup, Quantity, ProductType, Price, TotalPrice);
 	}
 }
---
+++ Task2/tests/Products.DomainTests/Model/PhoneTests.cs
@@ -0,0 +1,141 @@
+﻿using NUnit.Framework;
+
+namespace Products.Domain.Model.Tests
+{
+	[TestFixture()]
+	public class PhoneTests
+	{
+		[TestCase("ProductOne", 10, 1.5, 2, "Product", 15)]
+		[TestCase("ProductTwo", 20, 1.6, 5, "Product", 32)]
+		[TestCase("ProductThree", 30, 1.7, 6, "Product", 51)]
+		public void GivenGetPrice_WhenClassesFieldsIsPositive_ThenOutIsPriceProduct(
+			string productOneName, decimal productOneCost, double productOneMarkup, int productOneQuantity, string productOneProductType,
+			decimal expectedProductsPrice)
+		{
+			//Arrange
+			Phone productOne = new Phone(productOneName, productOneCost, productOneMarkup, productOneQuantity, productOneProductType);
+			//Act
+			decimal actualProductsPrice = productOne.Price;
+			//Assert
+			Assert.AreEqual(expectedProductsPrice, actualProductsPrice);
+		}
+
+		[TestCase("ProductOne", 10, 1.5, 2, "Product", 30)]
+		[TestCase("ProductTwo", 20, 1.6, 5, "Product", 160)]
+		[TestCase("ProductThree", 30, 1.7, 6, "Product", 306)]
+		public void GivenGetTotalPrice_WhenClassesFieldsIsPositive_ThenOutIsPriceProduct(
+			string productOneName, decimal productOneCost, double productOneMarkup, int productOneQuantity, string productOneProductType,
+			decimal expectedProductsPrice)
+		{
+			//Arrange
+			Phone productOne = new Phone(productOneName, productOneCost, productOneMarkup, productOneQuantity, productOneProductType);
+			//Act
+			decimal actualProductsTotalPrice = productOne.TotalPrice;
+			//Assert
+			Assert.AreEqual(expectedProductsPrice, actualProductsTotalPrice);
+		}
+
+		[TestCase("ProductOne", 10, 1.5, 2, "Product", "ProductOne", 20, 1.2, 3, "Product", "ProductOne", 16, 1.32, 5, "Product")]
+		[TestCase("ProductTwo", 20, 1.6, 5, "Product", "ProductTwo", 30, 1.3, 4, "Product", "ProductTwo", 24.444, 1.467, 9, "Product")]
+		[TestCase("ProductThree", 30, 1.7, 6, "Product", "ProductThree", 40, 1.4, 5, "Product", "ProductThree", 34.545, 1.564, 11, "Product")]
+		public void GivenOperatorSum_WhenClassesFieldsIsPositive_ThenOutIsPositive(
+			string productOneName, decimal productOneCost, double productOneMarkup, int productOneQuantity, string productOneProductType,
+			string productTwoName, decimal productTwoCost, double productTwoMarkup, int productTwoQuantity, string productTwoProductType,
+			string expectedName, decimal expectedCost, double expectedMarkup, int expectedQuantity, string expectedProductType
+			)
+		{
+			//Arrange
+			Phone productOne = new Phone(productOneName, productOneCost, productOneMarkup, productOneQuantity, productOneProductType);
+			Phone productTwo = new Phone(productTwoName, productTwoCost, productTwoMarkup, productTwoQuantity, productTwoProductType);
+			Phone expectedProduct = new Phone(expectedName, expectedCost, expectedMarkup, expectedQuantity, expectedProductType);
+			//Act
+			var actualProduct = productOne + productTwo;
+			//Assert
+			Assert.AreEqual(expectedProduct, actualProduct);
+		}
+
+		[TestCase("ProductOne", 10, 1.5, 3, "Product", 2, "ProductOne", 10, 1.5, 1, "Product")]
+		[TestCase("ProductOne", 15, 2, 10, "Product", 2, "ProductOne", 15, 2, 8, "Product")]
+		[TestCase("ProductOne", 20, 1, 15, "Product", 2, "ProductOne", 20, 1, 13, "Product")]
+		public void GivenOperatorMinus_WhenClassesFieldsIsPositive_ThenOutIsPositive(
+			string productOneName, decimal productOneCost, double productOneMarkup, int productOneQuantity, string productOneProductType,
+			int productsNumber,
+			string expectedName, decimal expectedCost, double expectedMarkup, int expectedQuantity, string expectedProductType
+			)
+		{
+			//Arrange
+			Phone productOne = new Phone(productOneName, productOneCost, productOneMarkup, productOneQuantity, productOneProductType);
+
+			Phone expectedProduct = new Phone(expectedName, expectedCost, expectedMarkup, expectedQuantity, expectedProductType);
+			//Act
+			var actualProduct = productOne - productsNumber;
+			//Assert
+			Assert.AreEqual(expectedProduct, actualProduct);
+		}
+
+		[TestCase("ProductOne", 10, 1.5, 2, "Product", "ProductOne", 20, 1.2, 3, "Product", "ProductOne", 20, 1.2, 3, "Product")]
+		[TestCase("ProductTwo", 20, 1.6, 5, "Product", "ProductTwo", 30, 1.3, 4, "Product", "ProductTwo", 30, 1.3, 4, "Product")]
+		[TestCase("ProductThree", 30, 1.7, 6, "Product", "ProductThree", 40, 1.4, 5, "Product", "ProductThree", 40, 1.4, 5, "Product")]
+		public void GivenOperatorPhoneToBook_WhenClassesFieldsIsPositive_ThenOutIsBook(
+			string productOneName, decimal productOneCost, double productOneMarkup, int productOneQuantity, string productOneProductType,
+			string productTwoName, decimal productTwoCost, double productTwoMarkup, int productTwoQuantity, string productTwoProductType,
+			string expectedName, decimal expectedCost, double expectedMarkup, int expectedQuantity, string expectedProductType
+			)
+		{
+			//Arrange
+			Phone productOne = new Phone(productOneName, productOneCost, productOneMarkup, productOneQuantity, productOneProductType);
+			Book productTwo = new Book(productTwoName, productTwoCost, productTwoMarkup, productTwoQuantity, productTwoProductType);
+			Phone expectedProduct = new Phone(expectedName, expectedCost, expectedMarkup, expectedQuantity, expectedProductType);
+			Phone actualProduct = productOne;
+			//Act
+			actualProduct = (Phone)productTwo;
+			//Assert
+			Assert.AreEqual(expectedProduct, actualProduct);
+		}
+
+		[TestCase("ProductOne", 10, 1.5, 2, "Product", 1500)]
+		[TestCase("ProductTwo", 20, 1.6, 5, "Product", 3200)]
+		[TestCase("ProductThree", 30, 1.7, 6, "Product", 5100)]
+		public void GivenOperatorInt_WhenClassesFieldsIsPositive_ThenOutIsCentsPriceProduct(
+			string productOneName, decimal productOneCost, double productOneMarkup, int productOneQuantity, string productOneProductType,
+			int expectedProductsPrice)
+		{
+			//Arrange
+			Phone productOne = new Phone(productOneName, productOneCost, productOneMarkup, productOneQuantity, productOneProductType);
+			//Act
+			int actualProductsPrice = (int)productOne;
+			//Assert
+			Assert.AreEqual(expectedProductsPrice, actualProductsPrice);
+		}
+
+		[TestCase("ProductOne", 10, 1.5, 2, "Product", 15)]
+		[TestCase("ProductTwo", 20, 1.6, 5, "Product", 32)]
+		[TestCase("ProductThree", 30, 1.7, 6, "Product", 51)]
+		public void GivenOperatorFloat_WhenClassesFieldsIsPositive_ThenOutIsFloatPriceProduct(
+			string productOneName, decimal productOneCost, double productOneMarkup, int productOneQuantity, string productOneProductType,
+			float expectedProductsPrice)
+		{
+			//Arrange
+			Phone productOne = new Phone(productOneName, productOneCost, productOneMarkup, productOneQuantity, productOneProductType);
+			//Act
+			float actualProductsPrice = (float)productOne;
+			//Assert
+			Assert.AreEqual(expectedProductsPrice, actualProductsPrice);
+		}
+
+		[TestCase("ProductOne", 10, 1.5, 2, "Product", 15)]
+		[TestCase("ProductTwo", 20, 1.6, 5, "Product", 32)]
+		[TestCase("ProductThree", 30, 1.7, 6, "Product", 51)]
+		public void GivenOperatorDouble_WhenClassesFieldsIsPositive_ThenOutIsDoublePriceProduct(
+			string productOneName, decimal productOneCost, double productOneMarkup, int productOneQuantity, string productOneProductType,
+			double expectedProductsPrice)
+		{
+			//Arrange
+			Phone productOne = new Phone(productOneName, productOneCost, productOneMarkup, productOneQuantity, productOneProductType);
+			//Act
+			double actualProductsPrice = (double)productOne;
+			//Assert
+			Assert.AreEqual(expectedProductsPrice, actualProductsPrice);
+		}
+	}
+}
