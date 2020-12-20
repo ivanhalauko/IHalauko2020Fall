9781739a83b674eb01c1953ad458db6a69227103
IvanHalauko <van_delta@mail.ru>
2020-12-20 13:28:25 +0300

    Task3. 02.Created IBaseShape, BaseShape and userException class.

---
+++ Task3/src/Shapes.Model.Lib/BaseShape.cs
@@ -0,0 +1,67 @@
+﻿using Shapes.Model.Lib.Interfaces;
+
+namespace Shapes.Model.Lib
+{
+	/// <summary>
+	/// Public base shape class for work. 
+	/// </summary>
+	public abstract class BaseShape : IBaseShape
+	{
+		/// <summary>
+		/// Property figure's area 
+		/// </summary>
+		public abstract double Area { get; }
+
+		/// <summary>
+		/// Property figure's perimetr
+		/// </summary>
+		public abstract double Perimeter { get; }
+
+		/// <summary>
+		/// Empty costructor.
+		/// </summary>
+		public BaseShape()
+		{
+		}
+
+		/// <summary>
+		/// Copy constructor.
+		/// </summary>
+		/// <param name="baseShape">Copy param.</param>
+		public BaseShape(BaseShape currentShape, BaseShape cuttingShape)
+		{
+			UserException.AreaCutting(currentShape, cuttingShape);
+		}
+
+		/// <summary>
+		/// Comparring the properties of two base shapes. Override Equals method for work with two base shapes.
+		/// </summary>
+		/// <param name="obj"></param>
+		/// <returns>Return "true" or "false" after comparer.</returns>
+		public override bool Equals(object obj)
+		{
+			if (obj == null || GetType() != obj.GetType())
+				return false;
+			BaseShape shape = (BaseShape)obj;
+			return Area.Equals(shape.Area) && Perimeter.Equals(shape.Perimeter);
+		}
+
+		/// <summary>
+		/// Hash calculation
+		/// </summary>
+		/// <returns>Return hash code.</returns>
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+
+		/// <summary>
+		/// Override base method ToString for get name of class.
+		/// </summary>
+		/// <returns>Return type's name.</returns>
+		public override string ToString()
+		{
+			return GetType().Name;
+		}
+	}
+}
