e035c362ad7531d55375708e65fd623755ca61fd
IvanHalauko <van_delta@mail.ru>
2020-12-20 15:39:54 +0300

    Task3. 05.Created FilmCircle, FilmRectangle, PaperCircle and PaperRectangle classes.

---
+++ Task3/src/Figures.Model.Lib/Figures/FilmCircle.cs
@@ -0,0 +1,28 @@
+﻿using Figures.Model.Lib.Interfaces;
+using Shapes.Model.Lib;
+
+namespace Figures.Model.Lib.Figures
+{
+	/// <summary>
+	/// Film circle figures class.
+	/// </summary>
+	public class FilmCircle : BaseCircleShape, IFilm
+	{
+		/// <summary>
+		/// Constructor with two parameters.
+		/// </summary>
+		/// <param name="radius">Radius parameter.</param>
+		public FilmCircle(double radius) : base(radius)
+		{
+		}
+
+		/// <summary>
+		/// Constructor to cut shape from another.
+		/// </summary>
+		/// <param name="currentShape">Shape's blank.</param>
+		/// <param name="cuttingShape">Shape which Cut out.</param>
+		public FilmCircle(BaseCircleShape currentShape, BaseCircleShape cuttingShape) : base(currentShape, cuttingShape)
+		{
+		}
+	}
+}
---
+++ Task3/src/Figures.Model.Lib/Figures/FilmRectangle.cs
@@ -0,0 +1,29 @@
+﻿using Figures.Model.Lib.Interfaces;
+using Shapes.Model.Lib;
+
+namespace Figures.Model.Lib.Figures
+{
+	/// <summary>
+	/// Film rectangle figures class.
+	/// </summary>
+	public class FilmRectangle : BaseRectangleShape, IFilm
+	{
+		/// <summary>
+		/// Constuctor with two parameters.
+		/// </summary>
+		/// <param name="length">Lenght parameter.</param>
+		/// <param name="width">Width parameter.</param>
+		public FilmRectangle(double length, double width) : base(length, width)
+		{
+		}
+
+		/// <summary>
+		/// Constructor to cut shape from another.
+		/// </summary>
+		/// <param name="currentShape">Shape's blank.</param>
+		/// <param name="cuttingShape">Shape which Cut out.</param>
+		public FilmRectangle(BaseRectangleShape currentShape, BaseRectangleShape cuttingShape) : base(currentShape, cuttingShape)
+		{
+		}
+	}
+}
