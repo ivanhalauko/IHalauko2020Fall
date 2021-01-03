3dd1f5b04d7a1703302c99dacd5feae11d5353de
IvanHalauko <van_delta@mail.ru>
2021-01-03 10:57:07 +0300

    Task4. 2. Created Client class and test for testing exception.

---
+++ Task4/tests/ClientServerLibTests/Model/ClientTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ClientServerLib.Model.Tests
+{
+    /// <summary>
+    /// Class for testing client class.
+    /// </summary>
+    [TestFixture()]
+    public class ClientTests
+    {
+        /// <summary>
+        /// Test case to testing exception.
+        /// </summary>
+        /// <param name="actualName">Client's name.</param>
+        /// <param name="actualIp">Client's Ip-adress.</param>
+        /// <param name="actualPort">Client's port.</param>
+        [TestCase("Ivan", "127.0.0.1", 8887)]
+        public void GivenClientWithoutServer_WhenNameIpPortIsNotNull_ThenOutIsException(
+            string actualName, string actualIp, int actualPort)
+        {
+            //Act
+            var ex = Assert.Throws<Exception>(() => new Client(actualName, actualIp, actualPort));
+            //Assert
+            Assert.That(ex.Message, Is.EqualTo("Connection Error"));
+        }
+    }   
+}
