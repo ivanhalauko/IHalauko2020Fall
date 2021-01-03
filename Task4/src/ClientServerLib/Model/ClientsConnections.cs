292f42481aa9870686adf0fa9bf7a579a239ab68
IvanHalauko <van_delta@mail.ru>
2021-01-03 11:47:14 +0300

    Task4. 3. Created ClientsConnections class and test for testing class.

---
+++ Task4/src/ClientServerLib/Model/ClientsConnections.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+namespace ClientServerLib.Model
+{
+    /// <summary>
+    /// Class create new connection on server.
+    /// </summary>
+    public class ClientsConnections
+    {
+        /// <summary>
+        /// Tcp client for to connect to the server.
+        /// </summary>
+        public TcpClient TcpClient { get; private set; }
+
+        /// <summary>
+        /// Client id which the server issues.
+        /// </summary>
+        public int ClientId { get; private set; }
+
+        /// <summary>
+        /// Through this object, you can send messages to the server or, conversely, receive data from the server.
+        /// </summary>
+        public NetworkStream NetworkStream { get; private set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="tcpClient">Tcp client for to connect to the server.</param>
+        /// <param name="server">The server to which the client is connected.</param>
+        /// <param name="clientId">Client id which the server issues.</param>
+        public ClientsConnections(TcpClient tcpClient, int clientId)
+        {
+            TcpClient = tcpClient;
+            ClientId = clientId;
+        }
+
+        /// <summary>
+        /// Method for exchanging messages with the client.
+        /// </summary>
+        public void OpenStream()
+        {
+            try
+            {
+                NetworkStream = TcpClient.GetStream();
+                byte[] data = new byte[64];
+                while (true)
+                {
+                    try
+                    {
+                        string message = "Message";                     
+                        data = Encoding.Unicode.GetBytes(message);
+                        NetworkStream.Write(data, 0, data.Length);
+                    }
+                    catch (Exception)
+                    {
+                        Debug.WriteLine("Client with id: " + ClientId + " disconected.");
+                        throw new Exception("Client with id: " + ClientId + " is not conected.");
+                       
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            finally
+            {
+                if (NetworkStream != null)
+                    NetworkStream.Close();
+
+                if (TcpClient != null)
+                    TcpClient.Close();
+            }
+        }
+    }
+}
---
+++ Task4/tests/ClientServerLibTests/Model/ClientsConnectionsTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.Net.Sockets;
+
+namespace ClientServerLib.Model.Tests
+{
+	/// <summary>
+	/// Class for testing ClientsConnections class.
+	/// </summary>
+	[TestFixture()]
+	public class ClientsConnectionsTests
+	{
+        /// <summary>
+        /// Test case to testing exception.
+        /// </summary>
+        /// <param name="hostname">Client's name.</param>
+        /// <param name="port">Client's Ip-adress.</param>
+        [TestCase("Ivan", 8887)]
+        public void GivenClientConnectionsWithoutServer_WhenClientIdTcpClientIsNotNull_ThenOutIsException(
+            string hostname, int port)
+        {
+            //Act
+            var ex = Assert.Throws<System.Net.Sockets.SocketException>(() => new TcpClient(hostname, port));
+            // var ex = Assert.Throws<Exception>(() => new ClientsConnections(tcpClient, clientId));
+            //Assert
+            //Assert.That(ex.Message, Is.EqualTo("Client with id: " + clientId + " is not conected."));
+            Assert.That(ex.Message, Is.EqualTo("Этот хост неизвестен"));
+        }
+    }
+}
