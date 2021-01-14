4b538dbf4f6684f6a0fe6b6c27a47c0cb08f5201
IvanHalauko <van_delta@mail.ru>
2021-01-14 22:13:00 +0300

    Task5 12. Created Models classes to describe student, Tests, TestsResult Entity..

---
+++ Task5/src/StudentInformationClass/Model/TestsInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StudentInformationClass
+{
+    /// <summary>
+    /// Class describe students tests information.
+    /// </summary>
+    [Serializable]
+    public class TestsInfo : Entity
+    {
+        /// <summary>
+        /// Property test's name.
+        /// </summary>
+        public string TestName { get; set; }
+    }
+}
---
+++ Task5/src/StudentInformationClass/Model/TestsResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StudentInformationClass
+{
+    /// <summary>
+    /// Class describing students test's results.
+    /// </summary>
+    [Serializable]
+    public class TestsResult : Entity
+    {
+        /// <summary>
+        /// Student's id property.
+        /// </summary>
+        public int IdStudent { get; set; }
+
+        /// <summary>
+        /// Student's id tests property.
+        /// </summary>
+        public int IdTest { get; set; }
+
+        /// <summary>
+        /// Student's tests rating property.
+        /// </summary>
+        public int StudentTestRating { get; set; }
+
+        /// <summary>
+        /// Date of Exams property.
+        /// </summary>
+        public DateTime DateOfExams { get; set; }
+    }
+}
