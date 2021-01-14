7e95da261883c392a9d9647bb02a11bf4b57fdfe
IvanHalauko <van_delta@mail.ru>
2021-01-14 22:27:41 +0300

    Task5 13. Created Repository class.

--- Task5/src/StudentInformationClass/Interfaces/IRepository.cs
+++ Task5/src/StudentInformationClass/Interfaces/IRepository.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace StudentInformationClass.Interfaces
 {
-	interface IRepository
+    /// <summary>
+    /// IRepository interface.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+	public interface IRepository<T>
 	{
-	}
+        /// <summary>
+        /// Method show node's of tree.
+        /// </summary>
+        /// <returns>Nodes of tree.</returns>
+        IEnumerable<T> ShowAllTree();
+
+        /// <summary>
+        /// Data add.
+        /// </summary>
+        /// <param name="obj">Oobject.</param>
+        void Add(T obj);
+
+        /// <summary>
+        /// Comparing one StudentTestResultRepository with another.
+        /// </summary>
+        /// <param name="obj">The compared StudentTestResultRepository.</param>
+        /// <returns>True if equal. False if not equal.</returns>
+        bool Equals(object obj);
+        
+        /// <summary>
+        /// Calculate hash code.
+        /// </summary>
+        /// <returns>The total hash code.</returns>
+        int GetHashCode();
+    }
 }
