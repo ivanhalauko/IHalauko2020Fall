94af8739f3d744a9aca8f29d528b49ee5c84dc80
IvanHalauko <van_delta@mail.ru>
2021-01-16 21:53:09 +0300

    Task6. 2.Created ModelInformation class with Substance, Students, Groupe, ExamTerms, ExamStudentResults, ExamForGroupe, and Exam. Added Auto comments to classe's properties.

---
+++ Task6/src/ModelsInformation/Models/ExamStudResults.cs
@@ -0,0 +1,38 @@
+﻿using ModelsInformation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModelsInformation.Models
+{
+    [Table("ExamStudentResults")]
+    /// <summary>
+    /// Class student's examine results.
+    /// </summary>
+    public class ExamStudResults : ISubstance
+    {
+        [Key]
+        /// <summary>
+        /// Implement interface ISubstance. 
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Student's ID property.
+        /// </summary>
+        public int IDStudent { get; set; }
+
+
+        /// <summary>
+        /// Student's groupe ID property.
+        /// </summary>
+        public int IDExamForGroupe { get; set; }
+
+
+        /// <summary>
+        /// Student's rating after exam property.
+        /// </summary>
+        public int Rating { get; set; }
+    }
+}
