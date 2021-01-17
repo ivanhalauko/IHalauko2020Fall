d717c49249a79ca6553c6dcc697881bd85d34699
IvanHalauko <van_delta@mail.ru>
2021-01-17 23:15:31 +0300

    Task6. 8.Created library project to read/write XLS files and tests.

---
+++ Task6/src/SQLServer.Task6.CvsReportManager.Services/CvsReportManager.cs
@@ -0,0 +1,43 @@
+﻿using SQLServer.Task6.CvsReportManager.Services.Interfaces;
+
+namespace SQLServer.Task6.CvsReportManager.Services
+{
+    /// <summary>
+    /// Cvs report manager.
+    /// </summary>
+    public class CvsReportManager
+    {
+        /// <summary>
+        /// Printer with IPrint type.
+        /// </summary>
+        public IPrint Printer { get; set; }
+
+        /// <summary>
+        /// Text in cvs format.
+        /// </summary>
+        public string CvsText { get; set; }
+
+        /// <summary>
+        /// Seporetor between words.
+        /// </summary>
+        public char Separator { get; set; }
+
+        /// <summary>
+        /// Constructor to initializing property.
+        /// </summary>
+        /// <param name="print">Print parameter.</param>
+        /// <param name="cvsText">Cvs text parameter.</param>
+        /// <param name="separator">Separator parameter.</param>
+        public CvsReportManager(IPrint print, string cvsText, char separator)
+        {
+            Printer = print;
+            CvsText = cvsText;
+            Separator = separator;
+        }
+
+        /// <summary>
+        /// Print text.
+        /// </summary>
+        public void Print() => Printer.Print(CvsText, Separator);
+    }
+}
---
+++ Task6/src/SQLServer.Task6.CvsReportManager.Services/Interfaces/IPrint.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SQLServer.Task6.CvsReportManager.Services.Interfaces
+{
+    /// <summary>
+    /// Intarface with print contract.
+    /// </summary>
+    public interface IPrint
+    {
+        /// <summary>
+        /// Print method.
+        /// </summary>
+        /// <param name="cvsString">String in cvs format.</param>
+        /// <param name="separator">Seporator between words.</param>
+        void Print(string cvsString, char separator);
+    }
+}
