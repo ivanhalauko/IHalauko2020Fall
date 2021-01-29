4abb7ec1639f3f50c7a3c59c223d8f65c494b944
IvanHalauko <van_delta@mail.ru>
2021-01-29 07:02:12 +0300

    Task7. 4. Created ModelsInformation lib for database.

---
+++ Task7/src/ModelsInformation/Models/Groups.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Linq.Mapping;
+using ModelsInformation.Interfaces;
+
+namespace ModelsInformation.Models
+{
+    /// <summary>
+    /// Student's groupe class.
+    /// </summary>
+    [Table(Name = "Groups")]
+    public class Groups : ISubstance
+    {
+        /// <summary>
+        /// Implement interface ISubstance. 
+        /// </summary>
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Student's groupe name property.
+        /// </summary>
+        [Column(Name = "GroupeName")]
+        public string GroupeName { get; set; }
+
+        /// <summary>
+        /// SpecialtiesId column in table.
+        /// </summary>
+        [Column(Name = "IDSpecialties")]
+        public int IDSpecialties { get; set; }
+    }
+}
