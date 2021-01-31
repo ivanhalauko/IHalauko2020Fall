29cc1210b9d4ee151a5f1466aaa9431dbdf9e2fe
IvanHalauko <van_delta@mail.ru>
2021-01-31 13:18:15 +0300

    Task7. 9. Created abstract class and interface to make view.

---
+++ Task7/src/SQLServerView/Interface/IView.cs
@@ -0,0 +1,36 @@
+﻿using ModelsInformation.Models;
+using System.Collections.Generic;
+
+namespace SQLServerView.Interface
+{
+    /// <summary>
+    /// Interface with tables contract.
+    /// </summary>
+    public interface IView
+    {
+        /// <summary>
+        /// Exams tables.
+        /// </summary>
+        IEnumerable<Subjects> Subjects { get; }
+        /// <summary>
+        /// Exams for groups tables.
+        /// </summary>
+        IEnumerable<ExamForGroup> ExamsForGroups { get; set; }
+        /// <summary>
+        /// Exam with students results tables.
+        /// </summary>
+        IEnumerable<ExamStudResults> ExamStudResults { get; }
+        /// <summary>
+        /// Exams's terms tables.
+        /// </summary>
+        IEnumerable<ExamTerms> ExamTerms { get; }
+        /// <summary>
+        /// Groups tables.
+        /// </summary>
+        IEnumerable<Groups> Groups { get; }
+        /// <summary>
+        /// Students tables.
+        /// </summary>
+        IEnumerable<Students> Students { get; }
+    }
+}
